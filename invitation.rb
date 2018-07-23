5bc4cfb143b16cb768d20721aac2584f9186024a
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-23 20:38:57 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -5,11 +5,13 @@ puts "What is the name of your pary?"
 party_name = gets.chomp
 puts "What is the date of your party?"
 date = gets.chomp
-rsvp = #{date} - 1
+puts = "When do you want them to RSVP?"
+rsvp = gets.chomp
 puts "What is the time of your party?"
 time = gets.chomp
 puts "What is the name of your host?"
 host_name = gets.chomp
+
 puts " Dear #{guest_name}, 
 
 You are cordially invited to the best #{party_name} party ever on #{date} at #{time}! Please RSVP no later than #{rsvp}. 
