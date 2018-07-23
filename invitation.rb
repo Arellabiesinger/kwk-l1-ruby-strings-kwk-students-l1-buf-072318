7d0e41ac4ddcef5d2dfe8ac0a8c87a0a63dcd5e2
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-23 20:21:15 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -5,8 +5,8 @@ puts "What is the name of your pary?"
 party_name = gets.chomp
 puts "What is the date of your party?"
 date = gets.chomp
-
-
+puts "What is the time of your party?"
+time = gets.chomp
 
 
 
