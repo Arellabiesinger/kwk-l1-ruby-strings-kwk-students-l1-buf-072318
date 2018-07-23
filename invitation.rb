50e926011b3adb8479de25847baa267e007ba61c
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-23 20:19:46 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,6 +1,6 @@
 # Code your prompts here!
 puts "What is your name?"
-geust_name = gets.chomp
+guest_name = gets.chomp
 puts "What is the name of your pary?"
 
 puts "What is the date of your party?"
